dce25c5a2ca37e41a3b0e9d55c519def50ebef23
Patrick James Garcia <pj_garcia_14@yahoo.com>
2017-05-05 14:01:15 +0800

    Set default strings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+        
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -79,15 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Adeltcode Portfolio
                 </div>
             </div>
         </div>
